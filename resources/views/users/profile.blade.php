b5301c54f754490732146ff8d394e92f103999dd
I-Kouta <kouta.baseball0222@gmail.com>
2022-09-26 23:27:31 +0900

    各要素の縦方向の余白をつくる記述

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -4,23 +4,34 @@
 
 {!! Form::open(['class' => 'profile-top']) !!}
 
-{{ Form::label('user-name', 'user name', ['class' => 'name']) }}
-{{ Form::text('username',null,['class' => 'input']) }}<br />
+<div class="category">
+  {{ Form::label('user-name', 'user name', ['class' => 'name']) }}
+  {{ Form::text('username',null,['class' => 'input']) }}
+</div>
+<div class="category">
+  {{ Form::label('address', 'mail address', ['class' => 'address']) }}
+  {{ Form::text('mail',null,['class' => 'input']) }}
+</div>
 
-{{ Form::label('address', 'mail address', ['class' => 'address']) }}
-{{ Form::text('mail',null,['class' => 'input']) }}<br />
+<div class="category">
+  {{ Form::label('password', 'password', ['class' => 'pass']) }}
+  {{ Form::password('password',['class' => 'input']) }}
+</div>
 
-{{ Form::label('password', 'password', ['class' => 'pass']) }}
-{{ Form::password('password',['class' => 'input']) }}<br />
+<div class="category">
+  {{ Form::label('password_confirmation', 'password confirm', ['class' => 'pass']) }}
+  {{ Form::password('password_confirmation',['class' => 'input']) }}
+</div>
 
-{{ Form::label('password_confirmation', 'password confirm', ['class' => 'pass']) }}
-{{ Form::password('password_confirmation',['class' => 'input']) }}<br />
+<div class="category">
+  {{ Form::label('bio', 'bio', ['class' => 'bio']) }}
+  {{ Form::password('password_confirmation',['class' => 'input']) }}
+</div>
 
-{{ Form::label('bio', 'bio', ['class' => 'bio']) }}
-{{ Form::password('password_confirmation',['class' => 'input']) }}<br />
-
-{{ Form::label('icon-image', 'icon image', ['class' => 'icon-image']) }}
-{{ Form::password('password_confirmation',['class' => 'input']) }}<br />
+<div class="category last">
+  {{ Form::label('icon-image', 'icon image', ['class' => 'icon-image']) }}
+  {{ Form::password('password_confirmation',['class' => 'input']) }}
+</div>
 
 {{ Form::submit('更新',['class' => 'red-btn']) }}
 
