8ed881f4cf65cd0262faa4ac3811541b57840d1f
I-Kouta <kouta.baseball0222@gmail.com>
2022-09-26 21:59:34 +0900

    ファサードへのクラスの付与、改行箇所の修正

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -2,27 +2,27 @@
 
 @section('content')
 
-{!! Form::open() !!}
+{!! Form::open(['class' => 'profile-top']) !!}
 
-{{ Form::label('user-name', 'user name', ['class' => 'name']) }}<br />
+{{ Form::label('user-name', 'user name', ['class' => 'name']) }}
 {{ Form::text('username',null,['class' => 'input']) }}<br />
 
-{{ Form::label('address', 'mail address', ['class' => 'address']) }}<br />
+{{ Form::label('address', 'mail address', ['class' => 'address']) }}
 {{ Form::text('mail',null,['class' => 'input']) }}<br />
 
-{{ Form::label('password', 'password', ['class' => 'pass']) }}<br />
+{{ Form::label('password', 'password', ['class' => 'pass']) }}
 {{ Form::password('password',['class' => 'input']) }}<br />
 
-{{ Form::label('password_confirmation', 'password confirm', ['class' => 'pass']) }}<br />
+{{ Form::label('password_confirmation', 'password confirm', ['class' => 'pass']) }}
 {{ Form::password('password_confirmation',['class' => 'input']) }}<br />
 
-{{ Form::label('bio', 'bio', ['class' => 'bio']) }}<br />
+{{ Form::label('bio', 'bio', ['class' => 'bio']) }}
 {{ Form::password('password_confirmation',['class' => 'input']) }}<br />
 
-{{ Form::label('icon-image', 'icon image', ['class' => 'icon-image']) }}<br />
+{{ Form::label('icon-image', 'icon image', ['class' => 'icon-image']) }}
 {{ Form::password('password_confirmation',['class' => 'input']) }}<br />
 
-{{ Form::submit('REGISTER',['class' => 'red-btn']) }}
+{{ Form::submit('更新',['class' => 'red-btn']) }}
 
 <!-- <p><a href="/login">ログイン画面へ戻る</a></p> -->
 
