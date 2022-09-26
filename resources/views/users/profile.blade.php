1aa76ba6fe16ca27aa78732326a078a30a20a688
I-Kouta <kouta.baseball0222@gmail.com>
2022-09-26 21:35:18 +0900

    プロフィール画面に名称、入力欄、登録ボタンを表示

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -2,6 +2,30 @@
 
 @section('content')
 
+{!! Form::open() !!}
 
+{{ Form::label('user-name', 'user name', ['class' => 'name']) }}<br />
+{{ Form::text('username',null,['class' => 'input']) }}<br />
+
+{{ Form::label('address', 'mail address', ['class' => 'address']) }}<br />
+{{ Form::text('mail',null,['class' => 'input']) }}<br />
+
+{{ Form::label('password', 'password', ['class' => 'pass']) }}<br />
+{{ Form::password('password',['class' => 'input']) }}<br />
+
+{{ Form::label('password_confirmation', 'password confirm', ['class' => 'pass']) }}<br />
+{{ Form::password('password_confirmation',['class' => 'input']) }}<br />
+
+{{ Form::label('bio', 'bio', ['class' => 'bio']) }}<br />
+{{ Form::password('password_confirmation',['class' => 'input']) }}<br />
+
+{{ Form::label('icon-image', 'icon image', ['class' => 'icon-image']) }}<br />
+{{ Form::password('password_confirmation',['class' => 'input']) }}<br />
+
+{{ Form::submit('REGISTER',['class' => 'red-btn']) }}
+
+<!-- <p><a href="/login">ログイン画面へ戻る</a></p> -->
+
+{!! Form::close() !!}
 
 @endsection
