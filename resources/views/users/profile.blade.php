7b01d636be2b5f3c418f0fa83570d9068d9e68bc
I-Kouta <kouta.baseball0222@gmail.com>
2022-10-01 22:52:16 +0900

    クラスの名前の変更

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -7,32 +7,32 @@
 <img class="user-image" src="images/icon1.png">
 <div class="category">
   {{ Form::label('user-name', 'user name', ['class' => 'name']) }}
-  {{ Form::text('username',Auth::user()->username,['class' => 'input']) }}
+  {{ Form::text('username',Auth::user()->username,['class' => 'output']) }}
 </div>
 
 <div class="category">
   {{ Form::label('address', 'mail address', ['class' => 'address']) }}
-  {{ Form::text('mail',Auth::user()->mail,['class' => 'input']) }}
+  {{ Form::text('mail',Auth::user()->mail,['class' => 'output']) }}
 </div>
 
 <div class="category">
   {{ Form::label('password', 'password', ['class' => 'pass']) }}
-  {{ Form::password('password',['class' => 'input']) }}
+  {{ Form::password('password',['class' => 'output']) }}
 </div>
 
 <div class="category">
   {{ Form::label('password_confirmation', 'password confirm', ['class' => 'pass']) }}
-  {{ Form::password('password_confirmation',['class' => 'input']) }}
+  {{ Form::password('password_confirmation',['class' => 'output']) }}
 </div>
 
 <div class="category">
   {{ Form::label('bio', 'bio', ['class' => 'bio']) }}
-  {{ Form::password('password_confirmation',['class' => 'input']) }}
+  {{ Form::password('password_confirmation',['class' => 'output']) }}
 </div>
 
 <div class="category">
   {{ Form::label('icon-image', 'icon image', ['class' => 'icon-image']) }}
-  {{ Form::file('image', ['class' => 'input file']) }}
+  {{ Form::file('image', ['class' => 'output file']) }}
 </div>
 
 {{ Form::submit('更新',['class' => 'red-btn']) }}
