b03657d3977838c853f48c1af619563782affb77
I-Kouta <kouta.baseball0222@gmail.com>
2022-10-02 18:14:38 +0900

    プロフィール更新時のバリデーションの記述

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,8 +10,18 @@ class UsersController extends Controller
     public function profile(){
         return view('users.profile');
     }
-    public function profileUpdate(){
-        return redirect('/top');
+    public function profileUpdate(Request $request){
+        if($request->isMethod('post')){
+            $data = $request->input(); // ここに入力したデータが入っている
+            $request->validate([
+                'username' => 'required|string|min:2|max:12',
+                'mail' => 'required|string|email|min:5|max:40|unique:users',
+                'password' => 'required|string|min:8|max:20|confirmed'
+            ]);
+            $this->create($data);
+            return redirect('/top'); // トップに移動する
+        }
+        return view('users.profile'); // 留まる
     }
 
     public function search(){
