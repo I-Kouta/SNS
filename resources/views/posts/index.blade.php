8939295b5279878c645c9e845a5a3440c8a02d41
I-Kouta <kouta.baseball0222@gmail.com>
2022-10-08 14:01:59 +0900

    属性の編集

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,7 +8,7 @@
 
 <div class="tweet-form">
   <img class="form-icon" src="images/icon1.png" width="35" height="35">
-  {{ Form::text('text',newPost,['class' => 'tweet', 'placeholder' => '投稿内容を入力してください']) }}
+  {{ Form::text('text',null,['class' => 'tweet', 'placeholder' => '投稿内容を入力してください']) }}
   <button type="submit" class="post-icon"><img src="images/post.png" width="70" height="70"></button>
 </div>
 
