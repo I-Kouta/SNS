28f3ccf987b219e199a3329e76a5cfb728b9480a
I-Kouta <kouta.baseball0222@gmail.com>
2022-10-11 22:33:55 +0900

    Authファサードを使うための記述

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PostsController extends Controller
 {
     //
+    public function __construct(){
+        $this->middleware('auth');
+    }
+
     public function index(){
         // ここにログインしたユーザー情報を書く
         return view('posts.index');
