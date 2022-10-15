19f3d267a397ad5a9c1ee4a4479ebb4a19babc2d
I-Kouta <kouta.baseball0222@gmail.com>
2022-10-15 12:09:20 +0900

    新規登録後の表示されるビューの修正

--- resources/views/auth/added.blade.php
+++ resources/views/auth/added.blade.php
@@ -3,13 +3,13 @@
 
 @section('content')
 
-<div id="clear">
+<div class="gray-back">
   <p>{{ session('username') }}さん</p>
   <p>ようこそ！AtlasSNSへ！</p>
   <p>ユーザー登録が完了しました。</p>
   <p>早速ログインをしてみましょう。</p>
 
-  <p class="btn"><a href="/login">ログイン画面へ</a></p>
+  <p class="btn red-btn"><a href="/login">ログイン画面へ</a></p>
 </div>
 
 @endsection
