f96606f272712da0b416e2c80816249c5f4a0f99
I-Kouta <kouta.baseball0222@gmail.com>
2022-10-23 15:28:03 +0900

    不要なタグの削除

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,13 +5,9 @@
 @section('content')
 
 {!! Form::open(['url' => 'post/create','class' => 'post-form']) !!}
-
-<div class="tweet-form">
   <img class="form-icon" src="images/icon1.png" width="35" height="35">
   {{ Form::input('text', 'newPost', null, ['required', 'class' => 'tweet', 'placeholder' => '投稿内容を入力してください', 'maxlength' => '150']) }}
   <button type="submit"><img src="images/post.png" width="100" height="100"></button>
-</div>
-
 {!! Form::close() !!}
 
 <div class='table'>
