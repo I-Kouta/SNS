517c64ae5f6ce57803e69858df4ea90839e1aff8
I-Kouta <kouta.baseball0222@gmail.com>
2022-10-23 16:20:26 +0900

    表示されているユーザーを横並び中央に表示させる記述

--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -7,9 +7,13 @@
   <button type="submit"><img src="images/post.png" width="100" height="100"></button>
   {!! Form::close() !!}
 
-@foreach ($user as $user)
-  <div>{{ $user->username }}</div>
-  <button type="submit" class="red-btn">フォロー</button>
-@endforeach
+<div class="search-form">
+  @foreach ($user as $user)
+  <div class="search-info">
+    <div>{{ $user->username }}</div>
+    <button type="submit" class="red-btn">フォロー</button>
+  </div>
+    @endforeach
+</div>
 
 @endsection
