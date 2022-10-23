790ba3da0afc4ca935384f7fb051560b7ace1a97
I-Kouta <kouta.baseball0222@gmail.com>
2022-10-23 14:37:14 +0900

    不要なメモの削除

--- routes/web.php
+++ routes/web.php
@@ -41,12 +41,10 @@ Route::group(["middleware" => "auth"], function() {
   Route::get('/profile','UsersController@profile');
   Route::post('/profile/update','UsersController@profileUpdate');
 
-  // edit, deleteは直接URL入力してもできないようにしたい
+  // edit, deleteは直接URL入力してもできないようしたいけど、特に実装は不要
   Route::post('post/update', 'PostsController@update');
 
   Route::get('post/{id}/delete', 'PostsController@delete');
-
-  // edit, deleteは直接URL入力してもできないようにしたい
 
   Route::get('/search','UsersController@index');
 
