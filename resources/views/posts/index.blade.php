3aa42806eae5235cfd8732e25150fc0a3c47c975
I-Kouta <kouta.baseball0222@gmail.com>
2022-10-23 14:52:50 +0900

    modal__bgとmodal__contentの親子関係の削除

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -28,15 +28,14 @@
         <a class="js-modal-open" href="" post="{{ $list->post }}" post_id="{{ $list->id }}"><img src="images/edit.png" width="30" height="30"></a> <!-- これは編集ボタン -->
         <!-- 編集内容が表示される -->
         <div class="modal js-modal">
-          <div class="modal__bg js-modal-close">
-            <div class="modal__content">
-              {!! Form::open(['url' => '/post/update']) !!}
-              {!! Form::hidden('id', $list->id, ['class' => 'modal_id']) !!}
-              {!! Form::input('text', 'upPost', $list->post, ['required', 'class' => 'modal_post']) !!}
-              <button type="submit"><img src="images/edit.png" width="30" height="30"></button>
-              {{ csrf_field() }}
-              {!! Form::close() !!}
-            </div>
+          <div class="modal__bg js-modal-close"></div>
+          <div class="modal__content">
+            {!! Form::open(['url' => '/post/update']) !!}
+            {!! Form::hidden('id', $list->id, ['class' => 'modal_id']) !!}
+            {!! Form::input('text', 'upPost', $list->post, ['required', 'class' => 'modal_post']) !!}
+            <button type="submit"><img src="images/edit.png" width="30" height="30"></button>
+            {{ csrf_field() }}
+            {!! Form::close() !!}
           </div>
         </div>
         <!-- 編集内容が表示される -->
