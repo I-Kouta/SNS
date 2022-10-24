49f9c05a1d0201a1b346967ecae202fdb285761e
I-Kouta <kouta.baseball0222@gmail.com>
2022-10-24 21:19:22 +0900

    インデントの修正

--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 
 {!! Form::open(['url' => 'searchResult','class' => 'post-form']) !!}
-  {{ Form::input('text', 'newPost', null, ['required', 'class' => 'search', 'placeholder' => 'ユーザー名']) }}
-  <button type="submit"><img src="images/post.png" width="100" height="100"></button>
-  {!! Form::close() !!}
+{{ Form::input('text', 'newPost', null, ['required', 'class' => 'search', 'placeholder' => 'ユーザー名']) }}
+<button type="submit"><img src="images/post.png" width="100" height="100"></button>
+{!! Form::close() !!}
 
 <div class="search-form">
   @foreach ($user as $user)
