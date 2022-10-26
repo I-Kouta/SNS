4f3bb2e86f56f814b0246f5eb2daeeb5176dae8f
I-Kouta <kouta.baseball0222@gmail.com>
2022-10-26 21:05:26 +0900

    検索結果を表示する欄の追加

--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -6,6 +6,9 @@
 <!-- inputタグ：type属性, name属性, 初期値, [それ以降] -->
 {{ Form::input('text', 'keyword', null, ['required', 'class' => 'search', 'placeholder' => 'ユーザー名']) }}
 <button type="submit"><img src="images/post.png" width="100" height="100"></button>
+<div class="">
+    <div>検索ワード：</div>
+  </div>
 {!! Form::close() !!}
 
 <div class="search-form">
