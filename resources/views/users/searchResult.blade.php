226fff504f0fe71b2ae7d992d120eb0257ac14a7
I-Kouta <kouta.baseball0222@gmail.com>
2022-10-27 22:21:52 +0900

    検索ワードを表示するための記述

--- resources/views/users/searchResult.blade.php
+++ resources/views/users/searchResult.blade.php
@@ -6,7 +6,7 @@
 {{ Form::input('text', 'keyword', null, ['required', 'class' => 'search', 'placeholder' => 'ユーザー名']) }}
 <button type="submit"><img src="images/post.png" width="100" height="100"></button>
 <div class="">
-  <div>検索ワード：</div>
+  検索ワード：{{ $keyword }}
 </div>
 {!! Form::close() !!}
 
