6a1c6de961168a244b3155c2b6824fa641f0d104
I-Kouta <kouta.baseball0222@gmail.com>
2022-10-29 13:17:15 +0900

    aタグのみでの表示

--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -19,9 +19,9 @@
   <div class="search-info">
     {{ $user->username }}
     <!-- フォローしていなければ -->
-    <button type="submit" class="blue-btn"><a href="/{{$user->id}}/follow">フォローする</a></button>
+    <a class="blue-btn" href="/{{$user->id}}/follow">フォローする</a>
     <!-- フォローしていたら -->
-    <button type="submit" class="red-btn"><a>フォロー解除</a></button>
+    <a class="red-btn">フォロー解除</a>
     <!-- end -->
   </div>
   @endif
