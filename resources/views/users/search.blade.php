843b1c63fb39cf532ae5a2ecc9aff424ce6ff6a8
I-Kouta <kouta.baseball0222@gmail.com>
2022-10-29 11:13:54 +0900

    検索キーワードの有無で条件分岐をするための記述

--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -6,6 +6,11 @@
 <!-- inputタグ：type属性, name属性, 初期値, [それ以降] -->
 {{ Form::input('text', 'keyword', null, ['required', 'class' => 'search', 'placeholder' => 'ユーザー名']) }}
 <button type="submit"><img src="images/post.png" width="100" height="100"></button>
+@if(!empty($keyword))
+<div class="search-word">
+  検索ワード：{{ $keyword }}
+</div>
+@endif
 {!! Form::close() !!}
 
 <div class="search-form">
