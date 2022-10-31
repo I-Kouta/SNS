36b77161a91a3cfdea35868573703c259d731aca
I-Kouta <kouta.baseball0222@gmail.com>
2022-10-31 23:01:53 +0900

    followsテーブルにフォローした・されたユーザーIDを追加するための記述

--- app/Http/Controllers/FollowsController.php
+++ app/Http/Controllers/FollowsController.php
@@ -3,13 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class FollowsController extends Controller
 {
     //
     public function follow($id){
         // followsテーブルに追加する記述
-        dd($id);
+        $user_id = Auth::id();
+        \DB::table('follows')->insert([
+            'following_id' => $user_id,
+            'followed_id' => $id
+        ]);
         return redirect('/search');
     }
 
