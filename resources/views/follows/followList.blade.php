a74049c4921393c58f265c117c62494df136cf6a
I-Kouta <kouta.baseball0222@gmail.com>
2022-11-07 19:58:00 +0900

    編集・削除に必要な記述の削除

--- resources/views/follows/followList.blade.php
+++ resources/views/follows/followList.blade.php
@@ -15,24 +15,6 @@
   </div>
   <div class="right-list">
     <div>{{ $list->updated_at }}</div>
-    @if(Auth::id() == $list->user_id)
-    <div class="update-edit">
-      <a class="js-modal-open" href="" post="{{ $list->post }}" post_id="{{ $list->id }}"><img src="images/edit.png" width="30" height="30"></a> <!-- これは編集ボタン -->
-      <!-- 編集内容が表示される -->
-      <div class="modal js-modal">
-        <div class="modal__bg js-modal-close"></div>
-        <div class="modal__content">
-          {!! Form::open(['url' => '/post/update']) !!}
-          {!! Form::hidden('id', $list->id, ['class' => 'modal_id']) !!}
-          {!! Form::input('text', 'upPost', $list->post, ['required', 'class' => 'modal_post']) !!}
-          <button type="submit"><img src="images/edit.png" width="30" height="30"></button>
-          {{ csrf_field() }}
-          {!! Form::close() !!}
-        </div>
-      </div>
-      <a href="/post/{{$list->id}}/delete" onclick="return confirm('この投稿を削除します。よろしいでしょうか？')"><img src="images/trash.png" width="30" height="30"></a>
-    </div>
-    @endif
   </div>
 </div>
 @endforeach
