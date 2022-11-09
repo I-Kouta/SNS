9a2f68a97e1c3ca350beee5c4707a3ce3c5342e4
I-Kouta <kouta.baseball0222@gmail.com>
2022-11-09 23:04:33 +0900

    該当する変数を繰り返す記述

--- resources/views/follows/followerList.blade.php
+++ resources/views/follows/followerList.blade.php
@@ -4,4 +4,16 @@
 
 <div class="post-form">Follower List</div>
 
+@foreach ($lists as $list)
+<div class="list">
+  <div class="left-list">
+    <div>{{ $list->user_name }}</div>
+    <div>{{ $list->post }}</div>
+  </div>
+  <div class="right-list">
+    <div>{{ $list->updated_at }}</div>
+  </div>
+</div>
+@endforeach
+
 @endsection
