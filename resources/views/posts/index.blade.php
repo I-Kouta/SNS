620a2e2bd06b68d31b90868559bb816d6a149ed1
I-Kouta <kouta.baseball0222@gmail.com>
2022-11-10 22:52:18 +0900

    仮アイコンの設置

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -13,8 +13,11 @@
 @foreach ($list as $list)
 <div class="list">
   <div class="left-list">
-    <div>{{ $list->user_name }}</div>
-    <div>{{ $list->post }}</div>
+    <img class="form-icon" src="images/icon1.png" width="35" height="35">
+    <div class="post-message">
+      <div>{{ $list->user_name }}</div>
+      <div>{{ $list->post }}</div>
+    </div>
   </div>
   <div class="right-list">
     <div>{{ $list->updated_at }}</div>
