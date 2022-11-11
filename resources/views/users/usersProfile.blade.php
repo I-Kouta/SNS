c21ac0f34f25fddc30823eb5ae226b8df7b8265a
I-Kouta <kouta.baseball0222@gmail.com>
2022-11-11 23:01:10 +0900

    プロフィールのテンプレの用意

---
+++ resources/views/users/usersProfile.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.login')
+
+@section('content')
+
+<div class="list">
+  <div class="left-list">
+    <img class="form-icon" src="images/icon1.png" width="35" height="35">
+    <div class="post-message">
+      <div>投稿者名</div>
+      <div>投稿内容</div>
+    </div>
+  </div>
+  <div class="right-list">
+    <div>投稿日時</div>
+  </div>
+</div>
+
+@endsection
