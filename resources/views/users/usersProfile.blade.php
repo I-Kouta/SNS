a2460c99f0ed44537a1cff02a24dbd82daf6b2ee
I-Kouta <kouta.baseball0222@gmail.com>
2022-11-11 23:15:37 +0900

    プロフィール上部の設置

--- resources/views/users/usersProfile.blade.php
+++ resources/views/users/usersProfile.blade.php
@@ -2,9 +2,15 @@
 
 @section('content')
 
+<div class="post-form">
+  <div class="post-message">
+      <div>name</div>
+      <div>bio</div>
+    </div>
+</div>
+
 <div class="list">
   <div class="left-list">
-    <img class="form-icon" src="images/icon1.png" width="35" height="35">
     <div class="post-message">
       <div>投稿者名</div>
       <div>投稿内容</div>
