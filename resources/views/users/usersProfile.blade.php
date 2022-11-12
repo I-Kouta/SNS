da7d6c5ce08d5fa82b000f51d233e0a249ab7793
I-Kouta <kouta.baseball0222@gmail.com>
2022-11-12 11:04:33 +0900

    特定のユーザーのusernameとbioを表示させる記述

--- resources/views/users/usersProfile.blade.php
+++ resources/views/users/usersProfile.blade.php
@@ -2,25 +2,14 @@
 
 @section('content')
 
+@foreach ($user as $user)
 <div class="post-form">
   <div class="post-message">
-    <div>name</div>
-    <div>bio</div>
-    <div class="search-form">
-      @foreach ($user as $user)
-      @if(Auth::id() != $user->id)
-      <div class="search-info">
-        @if (auth()->user()->isFollowing($user->id))
-        <p class="unFollow-btn"><a href="/{{$user->id}}/unFollow">フォロー解除</a></p>
-        @else
-        <p class="follow-btn"><a href="/{{$user->id}}/follow">フォローする</a></p>
-        @endif
-      </div>
-      @endif
-      @endforeach
-    </div>
+    <div>name{{ $user->username }}</div>
+    <div>bio{{ $user->bio }}</div>
   </div>
 </div>
+@endforeach
 
 <div class="list">
   <div class="left-list">
