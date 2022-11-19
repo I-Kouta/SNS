79d113d4fe78cb8a6cd7ff85e59f15e0b41e3c23
I-Kouta <kouta.baseball0222@gmail.com>
2022-11-19 11:30:28 +0900

    ファサードで送るデータの設定

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -4,21 +4,22 @@
 
 @foreach ($user as $user)
 <img class="user-image profile-icon" src="{{ asset('images/icon1.png') }}">
-{!! Form::open(['url' => '/profile/update','class' => 'profile-top']) !!}
 
+{!! Form::open(['url' => '/profile/update','class' => 'profile-top']) !!}
+{!! Form::hidden('id', $user->id) !!}
 <div class="category">
   {{ Form::label('user-name', 'user name', ['class' => 'name']) }}
-  {{ Form::text('username', $user->username,['class' => 'output']) }}
+  {{ Form::input('username', 'upUserName', $user->username,['class' => 'output']) }}
 </div>
 
 <div class="category">
   {{ Form::label('address', 'mail address', ['class' => 'address']) }}
-  {{ Form::text('mail', $user->mail,['class' => 'output']) }}
+  {{ Form::input('mail', 'upMail', $user->mail,['class' => 'output']) }}
 </div>
 
 <div class="category">
   {{ Form::label('password', 'password', ['class' => 'pass']) }}
-  {{ Form::password('password',['class' => 'output']) }}
+  {{ Form::password('password', ['class' => 'output']) }}
 </div>
 
 <div class="category">
@@ -28,18 +29,15 @@
 
 <div class="category">
   {{ Form::label('bio', 'bio', ['class' => 'bio']) }}
-  {{ Form::text('mail', $user->bio,['class' => 'output']) }}
+  {{ Form::input('mail', 'upBio', $user->bio,['class' => 'output']) }}
 </div>
 
 <div class="category">
   {{ Form::label('icon-image', 'icon image', ['class' => 'icon-image']) }}
   {{ Form::file('image', ['class' => 'output file']) }}
 </div>
-
-@endforeach
-
 {{ Form::submit('更新',['class' => 'red-btn']) }}
-
 {!! Form::close() !!}
+@endforeach
 
 @endsection
