78d3f4be1c440627ed13ec681405174363ca2253
I-Kouta <kouta.baseball0222@gmail.com>
2022-11-24 06:46:16 +0900

    投稿時の最大文字数の修正

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,7 +4,7 @@
 
 {!! Form::open(['url' => 'post/create','class' => 'post-form']) !!}
   <img class="form-icon" src="images/icon1.png" width="35" height="35">
-  {{ Form::input('text', 'newPost', null, ['required', 'class' => 'tweet', 'placeholder' => '投稿内容を入力してください', 'maxlength' => '150']) }}
+  {{ Form::input('text', 'newPost', null, ['required', 'class' => 'tweet', 'placeholder' => '投稿内容を入力してください', 'maxlength' => '200']) }}
   <button type="submit"><img src="images/post.png" width="100" height="100"></button>
 {!! Form::close() !!}
 
