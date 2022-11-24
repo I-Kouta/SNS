ab1dc8bb6b5c08cb2ac8ecb7a4f74e09b9d7460f
I-Kouta <kouta.baseball0222@gmail.com>
2022-11-25 00:21:12 +0900

    ファサードの引数の修正

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -9,12 +9,12 @@
 {!! Form::hidden('id', $user->id) !!}
 <div class="category">
   {{ Form::label('user-name', 'user name', ['class' => 'name']) }}
-  {{ Form::input('username', 'upUserName', $user->username,['class' => 'output']) }}
+  {{ Form::input('username', 'username', $user->username,['class' => 'output']) }}
 </div>
 
 <div class="category">
   {{ Form::label('address', 'mail address', ['class' => 'address']) }}
-  {{ Form::input('mail', 'upMail', $user->mail,['class' => 'output']) }}
+  {{ Form::input('mail', 'mail', $user->mail,['class' => 'output']) }}
 </div>
 
 <div class="category">
@@ -29,7 +29,7 @@
 
 <div class="category">
   {{ Form::label('bio', 'bio', ['class' => 'bio']) }}
-  {{ Form::input('bio', 'upBio', $user->bio,['class' => 'output']) }}
+  {{ Form::input('bio', 'bio', $user->bio,['class' => 'output']) }}
 </div>
 
 <div class="category">
